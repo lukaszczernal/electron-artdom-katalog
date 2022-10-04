c0024b9b364098d2bf0045765f66264a463a38c4
Lukasz <lukaszczernal@wp.pl>
2022-10-04 20:01:12 +0200

    App name change and initial setup fixes

--- electron/main/services/directories.ts
+++ electron/main/services/directories.ts
@@ -1,8 +1,8 @@
 import { existsSync, mkdirSync } from "fs";
 import { getPath } from "./env";
 
-const createDirectory = (path: string) => {
-  if (!existsSync(path)) {
+const createDirectory = (path?: string) => {
+  if (path && !existsSync(path)) {
     mkdirSync(path);
   }
 };
